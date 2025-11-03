4924cd7f5706bf911d49fae3acfd1320da2d4c9f
Matt Jenkinson <75292329+mattdjenkinson@users.noreply.github.com>
2025-11-03 20:46:16 +0000

    not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import ContentContainer from "@/components/content-container";
 import { Button } from "@/components/ui/button";
 import { ArrowLeftIcon } from "lucide-react";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <ContentContainer className="min-h-80">
+    <div className="mx-auto flex min-h-80 max-w-7xl flex-col items-start justify-center px-6 pt-10 pb-10 lg:px-8">
       <h1 className="text-2xl font-semibold">404 - Page Not Found</h1>
       <p className="mb-8">The page you are looking for does not exist.</p>
       <Link href="/" className="">
@@ -15,7 +14,7 @@ const NotFound = () => {
           <ArrowLeftIcon /> Go home
         </Button>
       </Link>
-    </ContentContainer>
+    </div>
   );
 };
 
